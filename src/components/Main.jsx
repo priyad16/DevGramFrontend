c00bff31202f3e49a85ed99076ecd7d65f01430b
priyad16 <priyadarshinikanchipuram@gmail.com>
2025-11-26 00:25:18 +0530

    add main compoenent

---
+++ src/components/Main.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import { Outlet } from "react-router";
+function Main(){
+    return(
+        <>
+        <Navbar/>
+        <Outlet/>
+        <Footer/>
+        </>
+    )
+}
+export default Main;
