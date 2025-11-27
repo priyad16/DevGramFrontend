5c6cac6f7d3c0c879414e2e798c540e3d964db18
priyad16 <priyadarshinikanchipuram@gmail.com>
2025-11-27 07:53:17 +0530

    add redux state using useSelector

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,43 @@
-function Navbar(){
-    return(
-        <div className="navbar bg-base-300 shadow-sm">
-  <div className="flex-1">
-    <a className="btn btn-ghost text-xl">DevGram</a>
-  </div>
-  <div className="flex-none">
-   
+import { useSelector } from "react-redux";
+
+function Navbar() {
+  const user = useSelector((state) => state.user);
+
+  return (
+    <div className="navbar bg-base-300 shadow-sm">
+      <div className="flex-1">
+        <a className="btn btn-ghost text-xl">DevGram</a>
+      </div>
+
+      {user && (
+  <div className="flex-none flex items-center gap-3 mr-5">
+
+    {/* 👋 Greeting text */}
+    <span className="font-medium text-base">
+      Hello, {user.firstName}
+    </span>
+
     <div className="dropdown dropdown-end">
-      <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar mr-5">
+      <div
+        tabIndex={0}
+        role="button"
+        className="btn btn-ghost btn-circle avatar"
+      >
         <div className="w-10 rounded-full">
           <img
-            alt="Tailwind CSS Navbar component"
-            src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+            alt="profile"
+            src={
+              user.avatar ||
+              "https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+            }
+          />
         </div>
       </div>
+
       <ul
         tabIndex="-1"
-        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+      >
         <li>
           <a className="justify-between">
             Profile
@@ -28,7 +49,10 @@ function Navbar(){
       </ul>
     </div>
   </div>
-</div>
-    )
+)}
+
+    </div>
+  );
 }
+
 export default Navbar;
