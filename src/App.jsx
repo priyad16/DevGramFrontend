c65c5e2d33d7cd5e4f8760f91090b1d8168dc554
priyad16 <priyadarshinikanchipuram@gmail.com>
2025-11-27 07:55:48 +0530

    add profile route

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Route, Routes } from "react-router";
 import Main from "./components/Main";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
+import Profile from "./components/Profile";
 function App() {
   return (
     <>
@@ -13,6 +14,7 @@ function App() {
             {/* <Route path="/" element={<Main />}></Route> */}
             <Route path="/login" element={<Login />}></Route>
             <Route path="/register" element={<Signup />}></Route>
+            <Route path="/profile" element={<Profile />}></Route>
           </Route>
         </Routes>
       </BrowserRouter>
