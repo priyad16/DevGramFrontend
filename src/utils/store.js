f7926a49ac4847e61d3fc6e341825de0d8b73331
priyad16 <priyadarshinikanchipuram@gmail.com>
2025-11-28 23:03:28 +0530

    add feed reducer in store

--- src/utils/store.js
+++ src/utils/store.js
@@ -1,9 +1,11 @@
 import {configureStore} from '@reduxjs/toolkit'
 import userReducer from './userslice'
+import feedReducer from './feedslice';
 const store=configureStore(
     {
         reducer:{
-            user:userReducer
+            user:userReducer,
+            feed:feedReducer
         },
 }
 )
